a88bc47fa8dbb925231998ecad2223ce2680f064
Jack Behar <jbehar@taylordigital.io>
2020-05-18 10:46:25 -0400

    pill extension

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,6 +7,8 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+import pill from 'netlify-cms-editor-component-pill';
+
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
@@ -14,3 +16,5 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+CMS.registerEditorComponent(pill);
